8f8b5e5a42a828869e0b2cdd77fd6a5fc84a8e6d
Kevin Duong <kdd007@bucknell.edu>
2025-02-03 03:30:36 -0500

    changed more ./ to /

--- quest2-starter.js
+++ quest2-starter.js
@@ -26,23 +26,23 @@
 // Chrome & Edge 113+ : Enable Vulkan, Default ANGLE Vulkan, Vulkan from ANGLE, Unsafe WebGPU Support, and WebGPU Developer Features (if exsits)
 // Firefox Nightly: sudo snap install firefox --channel=latext/edge or download from https://www.mozilla.org/en-US/firefox/channel/desktop/
 
-import FilteredRenderer from './lib/Viz/2DFilteredRenderer.js'
-import Standard2DFullScreenObject from './lib/DSViz/Standard2DFullScreenObject.js'
-import Standard2DPGAPosedVertexColorObject from './lib/DSViz/Standard2DPGAPosedVertexColorObject.js'
-// import LineStrip2DVertexObject from './lib/DSViz/LineStrip2DVertexObject.js'
-// import DemoTreeObject from './lib/DSViz/DemoTreeObject.js'
-import PGA2D from './lib/Math/PGA2D.js'
+import FilteredRenderer from '/lib/Viz/2DFilteredRenderer.js'
+import Standard2DFullScreenObject from '/lib/DSViz/Standard2DFullScreenObject.js'
+import Standard2DPGAPosedVertexColorObject from '/lib/DSViz/Standard2DPGAPosedVertexColorObject.js'
+// import LineStrip2DVertexObject from '/lib/DSViz/LineStrip2DVertexObject.js'
+// import DemoTreeObject from '/lib/DSViz/DemoTreeObject.js'
+import PGA2D from '/lib/Math/PGA2D.js'
 import circleMaker from './circleMaker.js'
 
-import rocketShip from './lib/DSViz/rocketShip.js'
-import mercury from './lib/DSViz/mercury.js'
-import venus from './lib/DSViz/venus.js'
-import earth from './lib/DSViz/earth.js'
-import mars from './lib/DSViz/mars.js'
-import jupiter from './lib/DSViz/jupiter.js'
-import saturn from './lib/DSViz/saturn.js'
-import uranus from './lib/DSViz/uranus.js'
-import neptune from './lib/DSViz/neptune.js'
+import rocketShip from '/lib/DSViz/rocketShip.js'
+import mercury from '/lib/DSViz/mercury.js'
+import venus from '/lib/DSViz/venus.js'
+import earth from '/lib/DSViz/earth.js'
+import mars from '/lib/DSViz/mars.js'
+import jupiter from '/lib/DSViz/jupiter.js'
+import saturn from '/lib/DSViz/saturn.js'
+import uranus from '/lib/DSViz/uranus.js'
+import neptune from '/lib/DSViz/neptune.js'
 import sunEffect from './sunEffect.js'
 
 async function init() {
